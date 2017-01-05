5b3aefb3488dd02b9613c2379435cbba784806ff
yangjian978 <896988974@qq.com>
2017-01-05 16:49:43 +0800

    提价

---
+++ index.android.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+
+var React = require('react-native');
+var {AppRegistry,} = React;
+
+var MainScreen = require('./MainScreen');
+
+AppRegistry.registerComponent('Demo3', () => MainScreen);
+
